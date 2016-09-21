7cfd7e9a55250edc3753e9fed7f9c5d21029b127
haorui <cdhaorui@jd.com>
2016-09-21 14:09:49 +0800

    加入request path 传统servlet的方式获取get参数

--- SpringMVCDrill/src/main/java/com/jd/risktest/Controller/HelloMvcController.java
+++ SpringMVCDrill/src/main/java/com/jd/risktest/Controller/HelloMvcController.java
@@ -1,17 +1,40 @@
 package com.jd.risktest.Controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloMvcController {
 	
-	@RequestMapping("/mvc")
-	public String helloMvc(Model model) {
-		model.addAttribute("qqq", "555");
+	private static Logger log=LoggerFactory.getLogger(HelloMvcController.class);
+	@RequestMapping("/Request")
+	public String helloMvc(@RequestParam("id") Integer id,Model model) {
+		log.debug("In viewCourse, courseId = {}", id);
+		model.addAttribute("qqq", id);
+		return "home";
+	}
+	
+	@RequestMapping(value="/Path/{id}",method=RequestMethod.GET)
+	public String helloMvc2(@PathVariable("id") Integer id,Model model) {
+		model.addAttribute("qqq", id);
 		return "home";
 	}
 
+	//传统方式
+	@RequestMapping("/old")
+	public String helloMvc3(HttpServletRequest request) {
+		Integer id=Integer.parseInt(request.getParameter("id"));
+		request.setAttribute("qqq", id);	
+		
+		return "home";
+	}
 }
