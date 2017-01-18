4f9fa54bd22c0262bc0228d419a531324d7405e6
DESKTOP-N9FJORV\gooo@live.cn <44478878@qq.com>
2017-01-18 21:00:52 +0800

    导入现有模板

--- SpringMVCDrill/src/main/java/com/jd/risktest/Controller/LoginController.java
+++ SpringMVCDrill/src/main/java/com/jd/risktest/Controller/LoginController.java
@@ -11,29 +11,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
-	@Autowired
-	public HttpSession session;
-	
-	@RequestMapping("/Index")
-	public String Index(HttpSession httpSession) {
-		return "index";
-	}
-	
-	
-	@RequestMapping("/Login")
-	public String toIndex(HttpSession httpSession) {
-		return "Login";
-	}
-	@RequestMapping("/doLogin")
-	public String doLogin(String userName, String password){
-		
-		if(Objects.equals(userName, "admin")&&Objects.equals(password, "123456")){
-			session.setAttribute("user", userName);
-			return "redirect:index";	
-		}else{
-			System.out.println(userName+":"+password);
-			return "redirect:Login";	
-		}
-		
-	}
+    @Autowired
+    public HttpSession session;
+
+    @RequestMapping("/index")
+    public String Index() {
+        return "index";
+    }
+
+    @RequestMapping("/uim")
+    public String uim() {
+        return "uim";
+    }
+
+    @RequestMapping("/Login")
+    public String toIndex() {
+        return "Login";
+    }
+
+    @RequestMapping("/doLogin")
+    public String doLogin(String userName, String password) {
+
+        if (Objects.equals(userName, "admin") && Objects.equals(password, "123456")) {
+            session.setAttribute("user", userName);
+            return "redirect:index";
+        } else {
+            System.out.println(userName + ":" + password);
+            return "redirect:Login";
+        }
+
+    }
 }
