6abe4538d08cf8c13866e7e5cbd26ddd107550be
haorui <cdhaorui@jd.com>
2017-03-20 14:08:42 +0800

    修改拦截器，放过静态资源

--- SpringMVCDrill/src/main/java/com/jd/risktest/Controller/LoginIntercepter.java
+++ SpringMVCDrill/src/main/java/com/jd/risktest/Controller/LoginIntercepter.java
@@ -6,32 +6,45 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 public class LoginIntercepter implements HandlerInterceptor {
 
-	@Override
-	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
-			throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
+    private List<String> excludedUrls;
 
-	@Override
-	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
-			throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
+            throws Exception {
+        // TODO Auto-generated method stub
 
-	@Override
-	public boolean preHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2) throws Exception {
-		   String url = arg0.getRequestURL().toString();
+    }
 
-		String loginPageUrl = "/Login";
-		if(!url.contains(loginPageUrl)&&!url.contains("/doLogin")&&arg0.getSession().getAttribute("user")==null){
-			arg0.getRequestDispatcher(loginPageUrl).forward(arg0, arg1);
-			return false;
-		}
-		return true;
-	}
+    @Override
+    public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
+            throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse arg1, Object arg2) throws Exception {
+
+        String requestUri = request.getRequestURI();
+        for (String url : excludedUrls) {
+            if (requestUri.contains(url)) {
+                return true;
+            }
+        }
+        String loginPageUrl = "/Login";
+        if (request.getSession().getAttribute("user") == null) {
+            request.getRequestDispatcher(loginPageUrl).forward(request, arg1);
+            return false;
+        }
+        return true;
+    }
+
+    public void setExcludedUrls(List<String> excludedUrls) {
+        this.excludedUrls = excludedUrls;
+    }
 
 }
