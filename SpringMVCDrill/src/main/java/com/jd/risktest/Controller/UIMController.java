643953a550925fc5b38860622c0bbabb7eec78ea
haorui <cdhaorui@jd.com>
2017-03-24 13:31:55 +0800

    update

--- SpringMVCDrill/src/main/java/com/jd/risktest/Controller/UIMController.java
+++ SpringMVCDrill/src/main/java/com/jd/risktest/Controller/UIMController.java
@@ -5,12 +5,16 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import com.jd.risktest.Service.*;
 @Controller
 @RequestMapping("/UIM")
 public class UIMController {
     @RequestMapping("/Index")
-    public String index() {
+    public String index(Map<String, Object> map) {
+        String reqId = "haorui7";
+        UimTest ut=new UimTest();
+        String ReponseText = ut.getUimReponseText(reqId);
+        map.put("uimResponse", ReponseText);
         return "uim";
     }
 
