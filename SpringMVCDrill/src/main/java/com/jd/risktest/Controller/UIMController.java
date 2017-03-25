ea5e34ca42ac6b8edaa24f95f0870b9ef0af8bd9
haorui658 <haorui658@gmail.com>
2017-03-25 15:50:26 +0800

    实现注入

--- SpringMVCDrill/src/main/java/com/jd/risktest/Controller/UIMController.java
+++ SpringMVCDrill/src/main/java/com/jd/risktest/Controller/UIMController.java
@@ -2,26 +2,30 @@ package com.jd.risktest.Controller;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.jd.risktest.Service.*;
+
 @Controller
 @RequestMapping("/UIM")
 public class UIMController {
+    @Autowired
+    UIMService uimService;
+
     @RequestMapping("/Index")
     public String index(Map<String, Object> map) {
         String reqId = "haorui7";
-        UimTest ut=new UimTest();
-        String ReponseText = ut.getUimReponseText(reqId);
-        map.put("uimResponse", ReponseText);
+        String ResponseText = uimService.getUim("risk2").getUimResponseText(reqId);
+        map.put("uimResponse", ResponseText);
         return "uim";
     }
 
     @RequestMapping("/query")
-    public String query(String resource, String pin,Map<String, Object> map) {
+    public String query(String resource, String pin, Map<String, Object> map) {
 
-        map.put("uimResponse", resource+pin);
+        map.put("uimResponse", resource + pin);
         return "uim";
     }
 }
